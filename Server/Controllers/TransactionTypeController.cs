95ecd878f11793c83a126a06b9e7ee3b9744e450
Delbert R. Hood <delbert.r.hood@nasa.gov>
2023-02-05 18:53:55 -0600

    SetupTT Controllers

---
+++ Server/Controllers/TransactionTypeController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBudget.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionTypeController : ControllerBase
+    {
+
+        private static List<TransactionType> TransactionTypes = new List<TransactionType>
+        {
+            new TransactionType
+            {
+                Id = 1,
+                TypeName = "Deposit",
+                Debit = false,
+                Description = "This is a Deposit Transation (+)"
+
+            },
+            new TransactionType
+            {
+                Id = 2,
+                TypeName = "Payment",
+                Debit = true,
+                Description = "This is a Paymenmt Transation (-)"
+
+            },
+            new TransactionType
+            {
+                Id = 3,
+                TypeName = "CashWithdrawl",
+                Debit = true,
+                Description = "This is a Withdrawl Payment Transation (-)"
+
+            },
+             new TransactionType
+            {
+                Id = 4,
+                TypeName = "CashDep",
+                Debit = false,
+                Description = "This is a Withdrawl Transation (+)"
+
+            },
+            new TransactionType
+            {
+                Id = 5,
+                TypeName = "CCPayment",
+                Debit = true,
+                Description = "This is a Credit Card Paymenmt Transation (-)"
+
+            },
+            new TransactionType
+            {
+                Id = 2,
+                TypeName = "Payroll",
+                Debit = false,
+                Description = "This is a Salary Transation (+)"
+
+            }
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<TransactionType>>> GetTransTypes()
+        {
+            return Ok(TransactionTypes);
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -11,7 +11,13 @@ StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configurat
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
+app.UseSwaggerUI();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -24,6 +30,8 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSwagger();
 
 app.UseHttpsRedirection();
 
