a664d0a65e57b97a2448853bc77aeeaac4359b7c
Delbert R. Hood <delbert.r.hood@nasa.gov>
2023-02-05 19:47:04 -0600

    Not Sure of Connection yet but. DbContext is set up with ConnectionString

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,12 +1,18 @@
 ﻿global using MyBudget.Shared;
+global using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting.StaticWebAssets;
 using Microsoft.AspNetCore.ResponseCompression;
+using MyBudget.Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
 
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
