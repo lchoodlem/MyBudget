1cd42ac7048637120336aa4f49f22e07c37e5be7
Delbert R. Hood <delbert.r.hood@nasa.gov>
2023-02-19 20:47:41 -0600

    YearMthMenus

--- Client/Enums/TabAdmin.cs
+++ Client/Enums/TabAdmin.cs
@@ -4,10 +4,12 @@ namespace MyBudget.Client.Enums
 {
     public enum TabAdmin
     {
-        Accounts = 0,
-        TransTypes = 1,
-        AcctTypes = 2,
-        StartDates = 3,
-        Organizations = 4
+        BudgetYears = 0,
+        Organizations = 1,
+        Accounts = 2, TransTypes = 3, AcctTypes = 4, StartDates = 5
+    }
+    public enum TabMonth
+    {
+        Jan = 1, Feb = 2, Mar = 3, Apr = 4, May = 5, Jun = 6, Jul = 7, Aug = 8, Sep = 9, Oct = 10, Nov = 11, Dec = 12
     }
 }
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,21 +1,15 @@
-global using Microsoft.JSInterop;
-global using MudBlazor;
 global using MudBlazor.Services;
 global using MyBudget.Client.Services.AccountService;
 global using MyBudget.Client.Services.AcctTypesService;
-global using MyBudget.Client.Services.OrganizationService;
-global using MyBudget.Client.Services.TransactionTypeService;
-global using MyBudget.Client.Services.StartDateAmountsService;
 global using MyBudget.Client.Services.BudgetYearService;
+global using MyBudget.Client.Services.OrganizationService;
+global using MyBudget.Client.Services.StartDateAmountsService;
+global using MyBudget.Client.Services.TransactionTypeService;
 global using MyBudget.Shared;
-global using MyBudget.Client.Enums;
 global using System.Net.Http.Json;
-
-global using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MyBudget.Client;
-
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -26,6 +20,8 @@ builder.Services.AddScoped<IStartDateAmountService, StartDateAmountService>();
 builder.Services.AddScoped<IOrganizationService, OrganizationService>();
 builder.Services.AddScoped<IAccountService, AccountService>();
 builder.Services.AddScoped<ITransactionTypeService, TransactionTypeService>();
+builder.Services.AddScoped<IBudgetYearService, BudgetYearService>();
+builder.Services.AddScoped<IAcctTypeService, AcctTypeService>();
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
